fb454f808defe50a8ce6928549ef9388e5c73fcc
Yash Rajput <yash92726@gmail.com>
2026-03-24 16:38:56 +0530

    Add files via upload

---
+++ android/build.gradle.kts
@@ -0,0 +1,6 @@
+// android/build.gradle.kts (Project-level)
+plugins {
+    id("com.android.application") version "8.3.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("com.google.gms.google-services") version "4.4.4" apply false
+}

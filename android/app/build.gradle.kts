cf6f563715e9fb67067e71f59e3e7e4fde279a64
Yash Rajput <yash92726@gmail.com>
2026-03-25 06:59:52 +0530

    Downgrade compileSdk and targetSdk to 35

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.yashsoftware.quiztech"
-    compileSdk = 36
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.yashsoftware.quiztech"
         minSdk = 23
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0.0"
         multiDexEnabled = true

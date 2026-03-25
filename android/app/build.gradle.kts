bf9f9b72c6a519ba25e9963aafe12010af53f0ee
Yash Rajput <yash92726@gmail.com>
2026-03-25 07:33:28 +0530

    Upgrade compileSdk and targetSdk to version 36

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.yashsoftware.quiztech"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.yashsoftware.quiztech"
         minSdk = 23
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0.0"
         multiDexEnabled = true

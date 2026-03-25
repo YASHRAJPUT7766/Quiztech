d55fa346466b80083f0a7a5549463386a9c0f7a3
Yash Rajput <yash92726@gmail.com>
2026-03-25 09:10:00 +0530

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,56 +1,18 @@
-plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("com.google.gms.google-services")
-    id("dev.flutter.flutter-gradle-plugin")
-}
-
-android {
-    namespace = "com.yashsoftware.quiztech"
-    compileSdk = 36
-
-    defaultConfig {
-        applicationId = "com.yashsoftware.quiztech"
-        minSdk = 23
-        targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
-        multiDexEnabled = true
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            isShrinkResources = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-        debug {
-            isMinifyEnabled = false
-            isShrinkResources = false
-        }
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
     }
 }
 
-flutter {
-    source = "../.."
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
 
-dependencies {
-    implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
-    implementation("com.google.firebase:firebase-auth")
-    implementation("com.google.firebase:firebase-firestore")
-    implementation("com.google.android.gms:play-services-auth:21.0.0")
-    implementation("androidx.multidex:multidex:2.0.1")
-}
+rootProject.name = "Quiztech"
+include(":app")
